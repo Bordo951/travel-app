0bb694b1a30ecc5682b5fbcfdad6493f55c160e0
Irina Selivanova <irina.selivanova1106@gmail.com>
2021-03-11 20:50:24 +0300

    TA-8:
    feat: add logo to the header

--- src/components/homePage/Logo.tsx
+++ src/components/homePage/Logo.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import styled from "styled-components";
 
-
+const LogoWrapper = styled.div`
+  width: 180px;
+  transition: transform .5s;  
+  &:hover {
+    transform: scale(1.05);
+  }  
+  a {
+    display: inline-block;
+  }  
+  img {
+    width: 100%;
+  }
+`;
 
 const Logo: React.FC = () => {
     return (
-        <Logo>
-            <div className="logo">
-                <a>
-                    <img/>
-                </a>
-            </div>
-        </Logo>
+        <LogoWrapper>
+            <a href='/'>
+                <img src='./images/logo.png' alt="logo"/>
+            </a>
+        </LogoWrapper>
     )
 }
 

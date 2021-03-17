2e97bf8405f74f47bb35b0b0e389489c6910c82e
Farrukh <fa.khusanov@gmail.com>
2021-03-17 20:00:25 +0500

    fix preloader

--- src/parts/Header.tsx
+++ src/parts/Header.tsx
@@ -6,38 +6,39 @@ import Autorization from "../components/homePage/Header/Autorization";
 import Location from "../components/homePage/Header/Location";
 
 const HeaderContentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px 50px;
+  background-color: #054b6d;
+  position: relative;
+  z-index: 110;
+  @media (max-width: 992px) {
+    padding: 5px 15px;
+  }
+
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  @media (max-width: 576px) {
     display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5px 50px;
-    background-color: #054B6D;
-    
-    @media (max-width: 992px) {
-        padding: 5px 15px;
-    }
-    
-    @media (max-width: 768px) {
-        flex-wrap: wrap;
-        justify-content: center;
-    }
-    
-    @media (max-width: 576px) {
-        display: flex;
-        flex-direction: column-reverse;
-    }
+    flex-direction: column-reverse;
+  }
 `;
 
 const Header: React.FC = () => {
-    return (
-        <header>
-            <HeaderContentContainer>
-                <LogoHeader/>
-                <Location/>
-                <Localization/>
-                <Autorization/>
-            </HeaderContentContainer>
-        </header>
-    )
+  return (
+    <header>
+      <HeaderContentContainer>
+        <LogoHeader />
+        <Location />
+        <Localization />
+        <Autorization />
+      </HeaderContentContainer>
+    </header>
+  );
 };
 
 export default Header;
